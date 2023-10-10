35efb5bf4b05d05bde2e6d00a33086606e24aee9
Trotteriniku <stefanozidda3@gmail.com>
2023-10-10 19:10:26 +0200

    errors in calculating scounts

--- js/script.js
+++ js/script.js
@@ -3,8 +3,20 @@ let btnload =document.getElementById('btn');
 btnload.addEventListener('click',
 function(){
     const nomeCompleto = document.getElementById('nomeCompleto').value;
-    const Km = document.getElementById('Km').value;
+    const Km = parseInt(document.getElementById('Km').value);
     const fasciaDieta = document.getElementById('fasciaDieta').value;
+    const prezzxKm = 0.21
 
-    
+    // if((nomeCompleto === null ||  Km === null &&  || fasciaDieta === null  )){
+    //     alert('fill all columns')
+        
+    // }
+
+   let prezzoTot = Km * prezzxKm
+   console.log(prezzoTot);
+
+   if (fasciaDieta < 18) {
+        prezzoTot = prezzoTot -(prezzoTot *20 / 100)    
+        console.log(prezzoTot); 
+   }
 });
