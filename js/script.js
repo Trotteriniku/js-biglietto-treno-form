e19510b7869ba7a386aa2f494bf801b5df5f3f47
Trotteriniku <stefanozidda3@gmail.com>
2023-10-10 18:30:25 +0200

    js variable descripted

---
+++ js/script.js
@@ -0,0 +1,10 @@
+let btnload =document.getElementById('btn');
+
+btnload.addEventListener('click',
+function(){
+    const nomeCompleto = document.getElementById('nomeCompleto').value;
+    const Km = document.getElementById('Km').value;
+    const fasciaDieta = document.getElementById('fasciaDieta').value;
+
+    
+});
